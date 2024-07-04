6c595858e8b371f32fa8afb632ae13958eb9eed7
tholiyaDixit <tholiyadixit1996@gmail.com>
2024-07-04 21:41:54 +0530

    add new page for image convert to base64

--- impact-front-end/src/pages/imageToUrl/ImageToUrl.tsx
+++ impact-front-end/src/pages/imageToUrl/ImageToUrl.tsx
@@ -1,48 +1,195 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import { styled, useTheme, Theme, CSSObject } from "@mui/material/styles";
+import "./ImageToUrl.scss";
+// import Button from "../../components/button/Button";
+import { Button, ButtonProps, buttonClasses } from "@mui/base/Button";
+import { LinearProgress } from "@mui/material";
 
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-end",
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-}));
+const ButtonRoot = React.forwardRef(function ButtonRoot(
+  props: React.PropsWithChildren<{}>,
+  ref: React.ForwardedRef<any>
+) {
+  const { children, ...other } = props;
+
+  return (
+    <svg width="150" height="50" {...other} ref={ref}>
+      <polygon points="0,50 0,0 150,0 150,50" className="bg" />
+      <polygon points="0,50 0,0 150,0 150,50" className="borderEffect" />
+      <foreignObject x="0" y="0" width="150" height="50">
+        <div className="content">{children}</div>
+      </foreignObject>
+    </svg>
+  );
+});
+
+const SvgButton = React.forwardRef(function SvgButton(
+  props: ButtonProps,
+  ref: React.ForwardedRef<any>
+) {
+  return <Button {...props} slots={{ root: CustomButtonRoot }} ref={ref} />;
+});
+
+const blue = {
+  50: "#F0F7FF",
+  100: "#C2E0FF",
+  200: "#99CCF3",
+  400: "#3399FF",
+  500: "#007FFF",
+  600: "#0072E6",
+  700: "#0059B3",
+  800: "#004C99",
+  900: "#003A75",
+};
+
+const CustomButtonRoot = styled(ButtonRoot)(
+  ({ theme }: { theme: Theme }) => `
+  overflow: visible;
+  cursor: pointer;
+  --main-color: ${theme.palette.mode === "light" ? blue[600] : blue[200]};
+  --hover-color: ${theme.palette.mode === "light" ? blue[50] : blue[900]};
+  --active-color: ${theme.palette.mode === "light" ? blue[100] : blue[800]};
+
+  & polygon {
+    fill: transparent;
+    transition: all 800ms ease;
+    pointer-events: none;
+  }
+
+  & .bg {
+    stroke: var(--main-color);
+    stroke-width: 1;
+    filter: drop-shadow(0 4px 16px rgba(0, 0, 0, 0.1));
+    fill: transparent;
+  }
+
+  & .borderEffect {
+    stroke: var(--main-color);
+    stroke-width: 2;
+    stroke-dasharray: 120 600;
+    stroke-dashoffset: 120;
+    fill: transparent;
+  }
+
+  &:hover,
+  &.${buttonClasses.focusVisible} {
+    .borderEffect {
+      stroke-dashoffset: -600;
+    }
+
+    .bg {
+      fill: var(--hover-color);
+    }
+  }
+
+  &:focus,
+  &.${buttonClasses.focusVisible} {
+    outline: 2px solid ${theme.palette.mode === "dark" ? blue[700] : blue[200]};
+    outline-offset: 2px;
+  }
+
+  &.${buttonClasses.active} {
+    & .bg {
+      fill: var(--active-color);
+      transition: fill 150ms ease-out;
+    }
+  }
+
+  & foreignObject {
+    pointer-events: none;
+
+    & .content {
+      font-size: 0.875rem;
+      font-family: 'IBM Plex Sans', sans-serif;
+      font-weight: 600;
+      line-height: 1.5;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: var(--main-color);
+    }
+
+    & svg {
+      margin: 0 4px;
+    }
+  }`
+);
 
 const ImageToUrl = () => {
+  const [baseImage, setBaseImage] = useState("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const uploadImage = async (e: any) => {
+    setLoading(true);
+    const file = e.target.files[0];
+    const base64: any = await convertBase64(file);
+    console.log("base64 ---", base64);
+
+    setBaseImage(base64);
+    setLoading(false);
+  };
+  console.log("loading ---", loading);
+
+  const convertBase64 = (file: any) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
+
   return (
     <Box component="main" sx={{ flexGrow: 1, p: 3 }} mt={2}>
       {/* <div> */}
-        {/* <DrawerHeader /> */}
-        <h1>ImageToUrl</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-          eveniet deserunt dolorum, sed, non architecto quo cumque repudiandae
-          obcaecati cupiditate ex voluptate voluptatibus voluptatem ipsam iure
-          nulla labore voluptas reiciendis? Quos corporis asperiores reiciendis
-          magni nemo modi mollitia, earum cumque pariatur officiis dolorem
-          fugiat similique molestiae enim ullam at excepturi doloremque! Ea sit,
-          fugit deserunt earum maiores quos, necessitatibus minima dolores
-          perferendis maxime ipsam voluptatum asperiores ab! Perferendis,
-          officia, minus similique asperiores sunt vero iure quasi facere
-          nesciunt obcaecati quisquam itaque corporis! Expedita earum possimus,
-          cupiditate odio minus rem commodi dolores maxime quidem deleniti
-          eveniet quasi aspernatur magni eligendi quisquam pariatur, nam est
-          dolore velit debitis inventore odit dignissimos non. Modi, quia!
-          Adipisci sapiente dolor, itaque delectus, corporis rem commodi
-          praesentium velit nihil quo facere inventore. Odio asperiores
-          doloribus blanditiis, culpa corporis a ullam et omnis nemo aspernatur
-          ex dolor. Cupiditate architecto aliquam ipsum fugiat. Laborum numquam
-          eligendi laboriosam rerum blanditiis quas fugit at! Laboriosam
-          deserunt nam voluptas nostrum doloremque eligendi nemo illum
-          reprehenderit maiores possimus. Cum voluptatem ut, ex aspernatur ab
-          sapiente, quod alias tempora doloremque atque, totam tenetur maxime.
-          Dolores molestias aperiam accusamus rerum consectetur saepe corporis!
-          Odit amet sit a modi obcaecati temporibus qui culpa ipsa expedita.
-        </p>
-      {/* </div> */}
+      {/* <DrawerHeader /> */}
+      <h1>ImageToUrl</h1>
+      <div className="imageUploadUrl">
+        <div className="uploadFile">
+          <input
+            type="file"
+            onChange={(e) => {
+              uploadImage(e);
+            }}
+          />
+        </div>
+        <div>
+          <div className="encodedUrl">
+            {!!baseImage ? (
+              <>
+                <div className="copyUrl">
+                  <SvgButton
+                    onClick={() => {
+                      {
+                        navigator.clipboard.writeText(baseImage);
+                      }
+                    }}
+                  >
+                    Copy
+                  </SvgButton>
+                </div>
+              </>
+            ) : (
+              <>
+                {loading && (
+                  <Box sx={{ width: "100%" }}>
+                    <LinearProgress />
+                  </Box>
+                )}
+              </>
+            )}
+
+            {baseImage}
+          </div>
+        </div>
+      </div>
     </Box>
   );
 };
