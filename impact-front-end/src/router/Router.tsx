dfe306828906af7ec092358ee92f691ecbaa8dd5
tholiyaDixit <tholiyadixit1996@gmail.com>
2024-07-06 19:13:09 +0530

    Remove extra code

--- impact-front-end/src/router/Router.tsx
+++ impact-front-end/src/router/Router.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SideBar from "../components/sideBar/SideBar";
 import ImageToUrl from "../pages/imageToUrl/ImageToUrl";
+import SignUp from "../pages/signUp/SignUp";
 
 const Router = () => {
   return (
@@ -10,6 +11,7 @@ const Router = () => {
       <SideBar></SideBar>
           <Routes>
             <Route path="/Image Base64" element={<ImageToUrl />} />
+            <Route path="/signUp" element={<SignUp />} />
           </Routes>
         </BrowserRouter>
     </>
