c342486e60457ed4c7b6965ce076b9d065fdefef
tholiyaDixit <tholiyadixit1996@gmail.com>
2024-07-06 23:15:34 +0530

    add home page

---
+++ impact-front-end/src/pages/home/Home.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react'
+import { useLocation } from 'react-router-dom';
+import usePopupManagement from '../../components/usePopupManagement';
+import CustomLogin from '../../popup/customLoginPopup/CustomLogin';
+
+const Home = () => {
+    const location = useLocation();
+    const data = location.state;
+    console.log("data ---",data.popup);
+    const PopupManagement = usePopupManagement(data)
+
+    // useEffect(() => {
+    //     !!data && !!data.popup && setCustomLoginPopup(true)
+    // },[])
+
+  return (
+    <div>
+       {PopupManagement}
+    </div>
+  )
+}
+
+export default Home
--- impact-front-end/src/router/Router.tsx
+++ impact-front-end/src/router/Router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SideBar from "../components/sideBar/SideBar";
 import ImageToUrl from "../pages/imageToUrl/ImageToUrl";
 import SignUp from "../pages/signUp/SignUp";
+import Home from "../pages/home/Home";
 
 const Router = () => {
   return (
@@ -10,6 +11,7 @@ const Router = () => {
       <BrowserRouter>
       <SideBar></SideBar>
           <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/Image Base64" element={<ImageToUrl />} />
             <Route path="/signUp" element={<SignUp />} />
           </Routes>
