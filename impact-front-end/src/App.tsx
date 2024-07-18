74ebb4ea7df285ee1e52a58554276b23335ba3bf
tholiyaDixit <tholiyadixit1996@gmail.com>
2024-07-18 06:05:18 +0530

    Call api with rtk query

--- impact-front-end/src/App.tsx
+++ impact-front-end/src/App.tsx
@@ -1,21 +1,45 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-// import SideBar from './components/sideBar/responsiveView/ResponsiveView';
-import SideBar from "./components/sideBar/SideBar";
 import Router from "./router/Router";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ImageToUrl from "./pages/imageToUrl/ImageToUrl";
-import CustomLogin from "./popup/customLoginPopup/CustomLogin";
+
+import { useLoginUserMutation } from "./redux/userApi/UserApi";
+import { useEffect } from "react";
 
 function App() {
+  let userData = {
+    email: "testing123@gmail.com",
+    password: "testing@213",
+  };
+  // const { data, error, isLoading } = useLoginUserMutation();  // for get method
+
+  const [
+    updatePost, // This is the mutation trigger
+    { isLoading: isUpdating, data: data }, // This is the destructured mutation result
+  ] = useLoginUserMutation();
+  console.log("allData ---", data);
+  console.log("isUpdating ---", isUpdating);
+
+  useEffect(() => {
+    const userLogin = async () => {
+      console.log("call ---");
+      
+      let userData = {
+        email: "testing123@gmail.com",
+        password: "testing@213",
+      };
+
+      await updatePost(userData);
+    };
+    userLogin()
+  }, []);
+
+  // console.log("data ---", data);
+  // console.log("error ---", error);
+  // console.log("isLoading ---", isLoading);
 
   return (
     <div className="App">
-     
       <Router />
-        {/* <CustomLogin /> */}
-         
+      {/* <CustomLogin /> */}
     </div>
   );
 }
